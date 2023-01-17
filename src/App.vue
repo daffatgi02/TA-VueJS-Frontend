211acb10c69af154ee0f791ebbb161699a6c74de
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-17 13:38:02 +0700

    [Update] - Changing template vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>User Management</h1>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Religion</th>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.nama }}</td>
+        <td>{{ user.umur }}</td>
+        <td>{{ user.agama }}</td>
+      </tr>
+    </table>
+    <button @click="getUsers">Get Users</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import axios from 'axios';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    async getUsers() {
+      try {
+        const response = await axios.get('http://localhost:3000/users');
+        this.users = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
-</style>
+
+</script>
