852651a833d3be7a0f2838e0132af02ed9a360a3
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-24 07:28:55 +0700

    [Change] - UI Homepage & Dashboard

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,11 +1,10 @@
 <template>
 
-    <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
-        style="background-image:url('https://pmb.ittelkom-sby.ac.id/cdn/environment/background.jpg');">
+    <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://pmb.ittelkom-sby.ac.id/cdn/environment/background.jpg');">
         <div class="max-w-4xl flex items-center lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
             <div id="profile" class="w-full rounded-lg shadow-2xl bg-white opacity-75 mx-6 lg:mx-20">
                 <div class="p-4 md:p-12 text-center lg:text-left">
-                    <h1 class="text-3xl font-bold pt-8 lg:pt-0">DATA MAHASISWA</h1>
+                    <h1 class="text-3xl text-center font-bold pt-8 lg:pt-0">DATA MAHASISWA TEKNIK KOMPUTER</h1>
                     <div class="mx-auto lg:mx-0 w-5/5 pt-2 border-b-2 border-red-500 opacity-25"></div>
                     <p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg
                             class="h-4 fill-current text-red-700 pr-4" xmlns="http://www.w3.org/2000/svg"
--- src/main/datamahasiswa.vue
+++ src/main/datamahasiswa.vue
@@ -1,35 +1,51 @@
 <template>
-    <div>
-      <h1>DATA MAHASISWA</h1>
-      <table>
-        <tr>
-          <th>NIM</th>
-          <th>NAMA</th>
-          <th>EMAIL</th>
-        </tr>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.nim }}</td>
-          <td>{{ user.nama }}</td>
-          <td>{{ user.email }}</td>
-        </tr>
-      </table>
+
+  <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
+    style="background-image:url('https://pmb.ittelkom-sby.ac.id/cdn/environment/background.jpg');">
+    <div class="max-w-4xl flex items-center lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+      <div id="profile" class="w-full rounded-lg shadow-2xl bg-white opacity-75 mx-6 lg:mx-20">
+        <div class="p-4 md:p-12 text-center lg:text-left">
+          <h1 class="text-3xl font-bold pt-8 lg:pt-0">KELAS CE-03-01</h1>
+          <div class="mx-auto">
+            <table class="w-full text-center">
+              <tr>
+                <th>NO</th>
+                <th>NIM</th>
+                <th>NAMA</th>
+                <th>EMAIL</th>
+              </tr>
+              <tr v-for="user in users" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.nim }}</td>
+                <td>{{ user.nama }}</td>
+                <td>{{ user.email }}</td>
+              </tr>
+            </table>
+          </div>
+
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
- import axios from 'axios';
-  export default {
-    name: 'datamahasiswa',
-    components:{},
-    data() {
-      return {
-        users: []
-      }
-    },
-    created() {
-      axios.get('http://localhost:3000/users')
-        .then(response => this.users = response.data)
-        .catch(error => console.error(error))
-    },
-  }
-  </script>
+  </body>
+
+
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'datamahasiswa',
+  components: {},
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get('http://localhost:3000/users')
+      .then(response => this.users = response.data)
+      .catch(error => console.error(error))
+  },
+}
+</script>
