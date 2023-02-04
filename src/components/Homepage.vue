c8eb86767055e95461b8561812e911aa8b37e336
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-02-04 10:43:57 +0700

    [UPDATED] - Change UI using bootstrap

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,30 +1,42 @@
 <template>
-    <head>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container h-100 d-flex justify-content-center align-items-center">
-        <div class="card text-center w-50 p-5 shadow-lg" style="background-color: rgba(255, 255, 255, 0.5);">
-          <div class="card-body">
-            <h1 class="text-white font-weight-bold">DATA MAHASISWA TEKNIK KOMPUTER</h1>
-            <hr class="w-25 mx-auto border-light mt-3">
-            <p class="text-white font-weight-bold">
-              <i class="fas fa-map-marker-alt text-light pr-2"></i>
-              INSTITUTTEKNOLOGI TELKOM SURABAYA
-            </p>
-            <p class="text-white font-weight-normal">
-              <i class="fas fa-map-marker-alt text-light pr-2"></i>
-              Jl. Ketintang No.156, Ketintang, Kec. Gayungan, Kota SBY, Jawa Timur 60231
-            </p>
-            <a href="/dashboard" class="btn btn-light mt-5">MASUK</a>
+    <div class="d-flex align-items-center" style="background-image: url(https://pmb.ittelkom-sby.ac.id/cdn/environment/background.jpg); background-size: cover; background-position: center; height: 100vh;">
+      <div class="container py-5">
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src="logo_kampus.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Logo Kampus
+          </a>
+        </nav>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header text-center">
+                <h3>Airtable Login</h3>
+              </div>
+              <div class="card-body">
+                <form>
+                  <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password">
+                  </div>
+                  <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </body>
+      <div style="background-color: rgba(0,0,0,0.7); position: absolute; top: 0; left: 0; width: 100%; height: 100%; filter: blur(5px);"></div>
+    </div>
   </template>
-  
-  
   
 <script>
 export default {
@@ -33,12 +45,5 @@ export default {
         return {};
     },
     methods: {},
-    created() { }
 };
 </script>
-<style>
-  body {
-    background-image: url('https://pmb.ittelkom-sby.ac.id/cdn/environment/background.jpg');
-    background-size: cover;
-  }
-</style>
