8ba400d7b9463853b767fadf7ca61139ccbcfd2c
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-02-05 14:24:15 +0700

    [UPDATE] - Add Validasi ketika hapus

--- src/main/Kelas1.vue
+++ src/main/Kelas1.vue
@@ -1,4 +1,5 @@
 <template>
+
   <body>
     <div class="container my-5">
       <h1 class="text-center mb-5">KELAS CE-03-01</h1>
@@ -25,13 +26,34 @@
                     <td>{{ user.email }}</td>
                     <td>
                       <button class="btn btn-warning btn-sm mr-2" @click="editUser(user.id)">Edit</button>
-                      <button class="btn btn-danger btn-sm" @click="deleteUser(user.id)">Hapus</button>
+                      <button class="btn btn-danger btn-sm bi bi-trash" @click="deleteUser(user.id)">Hapus</button>
                     </td>
                   </tr>
                 </tbody>
               </table>
               <button class="btn btn-primary btn-sm float-right my-3" @click="addUser">Tambah</button>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--notif delete-->
+    <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Hapus Data</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Apakah Anda yakin ingin menghapus data ini?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModal">Batal</button>
+            <button type="button" class="btn btn-danger" @click="confirmDelete">Hapus</button>
           </div>
         </div>
       </div>
@@ -47,8 +69,11 @@ export default {
       users: [],
       user: {
         id: "",
-        name: ""
-      }
+        nim: "",
+        nama: "",
+        email: ""
+      },
+      showModal: false
     };
   },
   mounted() {
@@ -63,21 +88,37 @@ export default {
         console.error(error);
       }
     },
-    async addUser() {
+    addUser() {
+      this.showModal = true;
+    },
+    closeModal() {
+      $("#confirmDeleteModal").modal("hide");
+    },
+    async saveUser() {
       try {
         const response = await axios.post("http://localhost:3000/users/", {
-          name: this.user.name
+          nim: this.user.nim,
+          nama: this.user.nama,
+          email: this.user.email
         });
         this.users.push(response.data);
-        this.user.name = "";
+        this.user.nim = "";
+        this.user.nama = "";
+        this.user.email = "";
+        this.showModal = false;
       } catch (error) {
         console.error(error);
       }
     },
     async deleteUser(id) {
+      $("#confirmDeleteModal").modal("show");
+      this.selectedId = id;
+    },
+    async confirmDelete() {
       try {
-        await axios.delete(`http://localhost:3000/users/${id}`);
-        this.users = this.users.filter(user => user.id !== id);
+        await axios.delete(`http://localhost:3000/users/${this.selectedId}`);
+        this.users = this.users.filter(user => user.id !== this.selectedId);
+        $("#confirmDeleteModal").modal("hide");
       } catch (error) {
         console.error(error);
       }
@@ -86,6 +127,7 @@ export default {
       try {
         const response = await axios.get(`http://localhost:3000/users/${id}`);
         this.user = response.data;
+        this.showModal = true;
       } catch (error) {
         console.error(error);
       }
@@ -93,11 +135,16 @@ export default {
     async updateUser() {
       try {
         await axios.put(`http://localhost:3000/users/${this.user.id}`, {
-          name: this.user.name
+          nim: this.user.nim,
+          nama: this.user.nama,
+          email: this.user.email
         });
         this.getUsers();
         this.user.id = "";
-        this.user.name = "";
+        this.user.nim = "";
+        this.user.nama = "";
+        this.user.email = "";
+        this.showModal = false;
       } catch (error) {
         console.error(error);
       }
@@ -105,3 +152,20 @@ export default {
   }
 };
 </script>
+
+
+<style>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.card {
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+.btn-sm {
+  padding: 0.25rem 0.5rem;
+  font-size: 0.7rem;
+}
+</style>
